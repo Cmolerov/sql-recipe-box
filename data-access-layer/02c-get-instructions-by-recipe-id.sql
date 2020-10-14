367d2ffbdd6e5c4ddd4784eff41aae92e92ac05a
carlos molero <cmolero130@gmail.com>
2020-10-14 16:10:26 -0400

    completed upto 04

--- data-access-layer/02c-get-instructions-by-recipe-id.sql
+++ data-access-layer/02c-get-instructions-by-recipe-id.sql
@@ -18,4 +18,9 @@
 -- WHERE ... (here you'll use the $1 parameter)
 
 
--- YOUR CODE HERE
+-- YOUR CODE HERE 
+
+SELECT list_order, specification 
+FROM instructions
+WHERE recipe_id = $1 
+ORDER BY list_order ASC;
