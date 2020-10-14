30a344e0f8feee5f908400eae5fe5e28564bb6c9
carlos molero <cmolero130@gmail.com>
2020-10-14 17:26:54 -0400

    finished delete and insert

--- data-access-layer/05-insert-new-instruction.sql
+++ data-access-layer/05-insert-new-instruction.sql
@@ -40,6 +40,16 @@
 --   «positional parameter»,
 --   «positional parameter»,
 -- )
-
+--   * Insert into the "instructions" table:
+--     * $1 is specification
+--     * $2 will contain the recipe id
 
 -- YOUR CODE HERE
+INSERT INTO instructions (list_order, specification, recipe_id)
+values 
+(
+   (SELECT COALESCE(MAX (list_order), 0) + 1 FROM instructions WHERE recipe_id = $2 ),
+   $1,
+   $2
+); 
+
